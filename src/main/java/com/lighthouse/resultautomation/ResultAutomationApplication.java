0c2a4de6ed08635a8eb9cd98a311c47e24bb7ac4
mdullash <mdullash460@gmail.com>
2019-07-11 21:39:58 +0600

    initial setup

---
+++ src/main/java/com/lighthouse/resultautomation/ResultAutomationApplication.java
@@ -0,0 +1,13 @@
+package com.lighthouse.resultautomation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResultAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResultAutomationApplication.class, args);
+	}
+
+}
